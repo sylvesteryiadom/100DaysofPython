b78b112c228070514f4c049a957036a7d8386795
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2021-01-04 21:53:21 -0600

    Day 4

--- day3.py
+++ day3.py
@@ -139,3 +139,47 @@
 #     print(f"Your score is {loveScore}, you are alright together.")
 # else:
 #     print(f"Your score is {loveScore}")
+
+# TREASURE ISLAND
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+leftORright = input('You\'re at a cross road. Where do you want to go? "left" or "right" ')
+
+if leftORright == 'left':
+    swimOrWait = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across ')
+    if swimOrWait == 'wait':
+        redYellowBlue = input("You arrive at the island unharmed. There is ahouse with 3 doors. One red, one yellow and one blue. Which color do you choose? ")
+        if redYellowBlue == 'yellow':
+            print("You win!")
+        elif redYellowBlue == 'blue':
+            print("Eaten by beasts. Game Over")
+        elif redYellowBlue == 'red':
+            print("Burned by fire. Game Over")
+        else:
+            print("Game Over")
+    elif swimOrWait !='wait':
+        print('Attacked by trout. Game Over.')
+else:
+    print("Fall into a hole. Game Over.")
---
+++ day4.py
@@ -0,0 +1,20 @@
+# Randomization and Python List
+import random
+
+# randomInteger = random.randint(1,10)
+# print(randomInteger)
+
+# # Random float
+# random_float = random.random() * random.randint(1,5)
+# print(random_float)
+
+random_number =  random.randint(0,1)
+
+if random_number == 0:
+    print('Heads')
+else:
+    print('Tails')
+
+
+# Python List - Similar to JS arrays
+
