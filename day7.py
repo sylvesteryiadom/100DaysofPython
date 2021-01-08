dc2bf302a80fd98ae98fe3af654ba69597957df4
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2021-01-07 23:59:11 -0600

    Day 7 Hangman

--- day7.py
+++ day7.py
@@ -1,9 +1,25 @@
 # HANGMAN
 import random
-
-
 words = ['banana', 'orange','melon','important']
 
-randomWord = words[random.randint(1,len(words)+1)]
-print(randomWord)
-userGuess = input("Guess a letter")
+# randomWord = words[random.randint(0,len(words))]
+chosenWord = random.choice(words)
+print(f"{chosenWord}")
+
+display = []
+
+for _ in chosenWord:
+    display += '_'
+
+while len(chosenWord) == len(display):
+    guess = input("Guess a letter: ").lower()[0] #capture 1 letter
+
+    for i in range(0,len(chosenWord)):
+        if guess == chosenWord[i]:
+            display[i] =chosenWord[i]
+    print(display)
+
+print('You win!')
+
+
+
