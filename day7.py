f3221d52ce36252b46d464c80c77e1a977e60f0b
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2021-01-08 00:38:03 -0600

    Day 7 Hangman

--- day7.py
+++ day7.py
@@ -1,25 +1,94 @@
 # HANGMAN
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 words = ['banana', 'orange','melon','important']
 
-# randomWord = words[random.randint(0,len(words))]
+
 chosenWord = random.choice(words)
 print(f"{chosenWord}")
-
+gameOver = False # state variable
+lives = 6
+# create blanks
 display = []
-
 for _ in chosenWord:
     display += '_'
 
-while len(chosenWord) == len(display):
-    guess = input("Guess a letter: ").lower()[0] #capture 1 letter
 
+
+while not gameOver:
+    guess = input("Guess a letter: ").lower()[0] #capture 1 letter
+    
     for i in range(0,len(chosenWord)):
         if guess == chosenWord[i]:
             display[i] =chosenWord[i]
+    # jump out of the if loop into the for loop and do the check there
+    if guess not in chosenWord:
+        lives -=1
+        if lives == 0:
+            gameOver = True
+            print('You Loose')
     print(display)
-
-print('You win!')
-
-
+    print(stages[lives])
+        
+    if '_' not in display:
+        gameOver = True # state variable
+        print("You Win")
+    
 
