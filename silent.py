d34ce3d168fd9757f7595a158da99bc5f9f8344f
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2021-01-18 15:44:22 -0600

    Day 10

--- silent.py
+++ silent.py
@@ -1,22 +1,20 @@
 import art
 print(art.logo)
 print("Welcome to the secret auction program")
-
-name = input("What is your name?: ")
-bid_amount = int(input("What's your bid?: "))
-
-# create empty dict and add to the dictionary
+# create empty dict and add to the dictionary Global variables
 auction_list = {}
-auction_list[name] = bid_amount
-userAnswer = input("Are there any other bidders? Type 'yes' or 'no'")
+bidding_over = False
 
-while userAnswer == 'yes':
+while not bidding_over:
   name = input("What is your name?: ")
-  bid_amount = int(input("What's your bid?: "))
+  bid_amount = int(input("What's your bid?: $"))
   auction_list[name] = bid_amount #append to dict
   userAnswer = input("Are there any other bidders? Type 'yes' or 'no'") #check for gameOver
+  if userAnswer =='no':
+      bidding_over = True
 
 max = 0;
+#sample dict {"Angela":100, "James":110}
 
 # loop to print autction and compare to max value
 for key in auction_list:
@@ -24,5 +22,5 @@ for key in auction_list:
 #   logic for determining max value
   if amount > max:
     max = amount
-    
-print(f"{key} is the winner of the auction with an amount of {amount}")
+
+print(f"{key} is the winner of the auction with an amount of ${max}")
